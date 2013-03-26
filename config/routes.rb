0a8452b2619ccb82e88aacbb99bd8b27de786b77
Jason Ronallo <jronallo@gmail.com>
2013-03-26 11:39:33 -0700

    update root path

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ ElasticMicrodata::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'itemtypes#index'
 
   # See how all your routes lay out with "rake routes"
 
